590e4a5d945a6968f732406ada059326e0b27706
ManuelBeltran <manuel.beltran@sofiaxt.com>
2026-05-09 22:54:51 -0700

    fix: use https for OMDB API calls

--- src/proyecto1_movies/services/movieService.ts
+++ src/proyecto1_movies/services/movieService.ts
@@ -3,14 +3,14 @@ import { Movie, MovieDetails } from "../types";
 const VITE_OMDB_KEY = import.meta.env.VITE_OMDB_KEY;
 
 export async function searchMovies(query: string): Promise<Movie> {
-  const searchUrl = `http://www.omdbapi.com/?s=${query}&apikey=${VITE_OMDB_KEY}`;
+  const searchUrl = `https://www.omdbapi.com/?s=${query}&apikey=${VITE_OMDB_KEY}`;
   const res = await fetch(searchUrl);
   if (!res.ok) throw new Error("Error");
   return res.json();
 }
 
 export async function detailsMovies(id: string): Promise<MovieDetails> {
-  const detailUrl = `http://www.omdbapi.com/?i=${id}&apikey=${VITE_OMDB_KEY}`;
+  const detailUrl = `https://www.omdbapi.com/?i=${id}&apikey=${VITE_OMDB_KEY}`;
   const res = await fetch(detailUrl);
   if (!res.ok) throw new Error("Error");
   return res.json();
